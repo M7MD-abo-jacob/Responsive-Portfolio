63a3e25efc94d8cbeb87ff246c0b6d8945d11159
M7MD-abo-jacob <105122190+M7MD-abo-jacob@users.noreply.github.com>
2022-05-29 21:39:49 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,38 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+
+// toggle night mode
+function toggleNight(){
+  var nightBtn = document.getElementById('night-mode');
+  var body = document.getElementsByTagName('body')[0];
+  body.classList.toggle("dark");
+  nightBtn.classList.toggle("fa-moon");
+  nightBtn.classList.toggle("fa-sun");
+}
